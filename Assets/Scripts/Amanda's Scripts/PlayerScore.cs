0b39522e0ae0c03fb26379d493a48a705af2b412
Amanda <windsoramanda@outlook.com>
2023-08-08 09:22:15 +1000

    New scene additions
    Refined game flow with addition of Lair Sweet Lair and Back Story scene

---
+++ Assets/Scripts/Amanda's Scripts/PlayerScore.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace PlayerScoreSystem {
+
+    public class PlayerScore {
+
+    
+
+    // Variable for Villain Point total
+    int villainPointCount = 0;
+    // Variable for Current Party Score
+    int currentPartyScore = 0;
+    
+    }
+}
